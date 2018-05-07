7bf5898e33e3a48b0816b6628ac729e29227b446
Isaac Bigsby Trogdon <mr@dyss.net>
2018-05-07 11:03:31 +0200

    Commit theme files.

---
+++ README.md
@@ -0,0 +1,18 @@
+Install drupal 8
+
+composer global require hussainweb/drupal-composer-init
+
+composer drupal-init (TODO or download custom composer)
+
+composer install
+
+(add to devdesktop)
+
+drush si
+
+drush upwd admin password
+
+Uncomment lines settings.local.php in settings.php
+
+Enable custom_updates, then:
+drush updb
--- www/themes/gal1base_ui/js/scripts.js
+++ www/themes/gal1base_ui/js/scripts.js
@@ -6,11 +6,11 @@
    * @param {*}
    */
 
-  Drupal.behaviors.base8_ui = {
+  Drupal.behaviors.gallery1_ui = {
     attach: function (context, settings) {
-    $('body', context).once('base8_ui').each(function () {
-      console.log('d8base.js');
-    });
+      $('body', context).once('gallery1_ui').each(function () {
+        console.log('gallery1_ui/scripts.js');
+      });
     }
   };
   })(jQuery);
