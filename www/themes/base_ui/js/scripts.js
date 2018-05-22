9bac974800965926705cbdc6d4489a9ce469b837
Isaac Bigsby Trogdon <mr@dyss.net>
2018-05-22 16:13:31 +0200

    Layout fixes, nav position, and sticky footer.

--- www/themes/base_ui/js/scripts.js
+++ www/themes/base_ui/js/scripts.js
@@ -6,30 +6,11 @@
    * @param {*}
    */
 
-  /**
-   * Initialize off-canvas menu.
-   */
-  Drupal.behaviors.offCanvasMenuInit = {
-    attach: function (context, settings) {
-      $('.main-menu', context).once('offCanvasMenuInit').each(function () {
-        // var $menu = $(this);
-        // var $offCanvas = $('<nav>');
-        // var $menuClone = $menu.find('> ul').clone().removeAttr('class').addClass('off-canvas-menu-item');
-        // var $burger = $('<img src="/themes/d8base_ui/images/burger.svg" alt="Burger menu icon for launching menu" aria-hidden burger>');
-        // $offCanvas.insertBefore($menu)
-        //   .append($burger)
-        //   .append($menuClone)
-        //   .addClass('main-menu header-block off-canvas-menu menu-closed')
-        //   .attr('off-canvas-menu', '')
-        //   .attr('off-canvas-status', 1);
-      });
-    }
-  };
 
   /**
    * Position off-canvas menu.
    */
-  Drupal.behaviors.offCanvasMenuPosition = {
+  Drupal.behaviors.offCanvasMenu = {
     attach: function (context, settings) {
 
       function offCanvasMenuPosition() {
@@ -41,28 +22,34 @@
           let status = 0;
           let offset = round($menu.outerWidth() * status, 0) + 1;
           // Init position - translateX of menu-width.
-          $menu.css({
-            // 'left': offset + 'px',
-            // 'transform': 'translateX(' + offset + 'px)',
-            'opacity': status,
-          });
+          // $menu.css({
+          //   // 'left': offset + 'px',
+          //   // 'transform': 'translateX(' + offset + 'px)',
+          //   'opacity': status,
+          // });
 
-          function toggleMenu($menu) {
-            let status = $menu.parent().attr('off-canvas-status');
+          function toggleMenu($menu, status = null) {
+            if (status !== null) {
+              status = 0;
+              console.log('override')
+            }
+            else {
+              status = $menu.parent().attr('off-canvas-status');
+              status = status == 0 ? 1 : 0;
+            }
             // invert status
-            status = status == 0 ? 1 : 0;
             let offset = round($menu.outerWidth() * status * -1, 0) + 1;
-            $menu.velocity({
-              // 'translateX': (offset * -1) + 'px',
-              'opacity': status,
-            }, {
-              duration: 50,
-              easing: 'easein',
-            });
+            // $menu.velocity({
+            //   // 'translateX': (offset * -1) + 'px',
+            //   'opacity': status,
+            // }, {
+            //   duration: 50,
+            //   easing: 'easein',
+            // });
             $menu.parent().attr('off-canvas-status', status);
             $menu.parent().toggleClass('menu-closed');
             $('body').toggleClass('off-canvas-menu-active');
-            let $logo = $('.site-branding');
+            // let $logo = $('.site-branding');
             $canvas.toggleClass('active');
           }
 
@@ -72,9 +59,10 @@
           });
           let $canvas = $('#menu-canvas');
           $canvas.click(function(e){
-            console.log('menu = ' + $menu);
-            toggleMenu($menu);
+            toggleMenu($menu, 0);
           });
+
+
         });
       }
 
@@ -97,7 +85,9 @@
         // console.log('resizing');
         waitForFinalEvent(function() {
           offCanvasMenuPosition();
+          // $menu.attr('off-canvas-status', 0);
         }, 500);
+        $('[menu-mobile]').addClass('menu-closed').attr('off-canvas-status', 0);
       });
 
 
@@ -213,64 +203,86 @@
     attach: function (context, settings) {
       $('#main-navigation nav[menu-desktop]', context).once('dropdownMenu').each(function () {
 
-        function toggleDropdown($menu, $submenu, $link, clickEvent, override = null) {
-          if (override == -1) {
-            $menu.find('a.active').removeClass('active');
-            $menu.find('ul.submenu-open').removeClass('submenu-open');
-            $menu.attr('dropdown-active', 0);
-            $canvas.removeClass('active');
-            return false;
-          }
-          // console.log('submenu maybe')
-          // console.log($submenu);
-          if ($submenu.length != 0) {
 
-            clickEvent.preventDefault();
-
-            // Toggle out any currently active menus.
-            let $siblings = $link.parent().siblings('li');
-            $siblings.find('a.active').removeClass('active');
-            $siblings.find('ul.submenu-open').removeClass('submenu-open');
-            // Toggle active submenu.
-            $link.toggleClass('active');
-            $submenu.toggleClass('submenu-open');
-            // Global menu settings.
-            if($menu.find('a.active').length > 0) {
-              $menu.attr('dropdown-active', 1);
-              $canvas.addClass('active')
-            }
-            else {
-              $menu.attr('dropdown-active', 0);
-              $canvas.removeClass('active')
-            }
-          }
-        }
 
         let $menu = $(this);
         let $canvas = $('#menu-canvas');
-        console.log($(this));
 
         $menu.attr('dropdown-active', 0);
         $menu.find('li a').click(function(e){
           // let $link = $(this);
           let $link = $(this).parent().children('a');
           let $submenu = $(this).parent().children('ul');
-          console.log($link);
-          console.log($submenu);
+          // console.log($link);
+          // console.log($submenu);
           // If submenu exists under this link, toggle open.
-          e.preventDefault();
-          toggleDropdown($menu, $submenu, $link, e);
+          // e.preventDefault();
+          toggleDropdown($menu, $submenu, $link, e, null, $);
         });
 
         $canvas.click(function(e){
-          toggleDropdown($menu, false, false, e, -1);
+          toggleDropdown($menu, false, false, e, -1, $);
         });
 
       });
     }
   }
 
+  /**
+   *
+   */
+  Drupal.behaviors.bodyActions = {
+    attach: function (context, settings) {
+      $('body', context).once('bodyActions').each(function () {
+        $(window).resize(function(e){
+          toggleDropdown($('#main-navigation nav[menu-desktop]'), false, false, e, -1, $);
+        });
+
+
+      });
+    }
+  };
+
+
+
   })(jQuery);
+
+
+  function toggleDropdown($menu, $submenu, $link, clickEvent, override = null, $) {
+    var $canvas = $('#menu-canvas');
+    // var $canvas = document.getElementById('#menu-canvas');
+    if (override == -1) {
+      $menu.find('a.active').removeClass('active');
+      $menu.find('ul.submenu-open').removeClass('submenu-open');
+      $menu.attr('dropdown-active', 0);
+      $canvas.removeClass('active');
+      return false;
+    }
+    // console.log('submenu maybe')
+    // console.log($submenu);
+    if ($submenu.length != 0) {
+
+      clickEvent.preventDefault();
+
+      // Toggle out any currently active menus.
+      let $siblings = $link.parent().siblings('li');
+      $siblings.find('a.active').removeClass('active');
+      $siblings.find('ul.submenu-open').removeClass('submenu-open');
+      // Toggle active submenu.
+      $link.toggleClass('active');
+      $submenu.toggleClass('submenu-open');
+      // Global menu settings.
+      if($menu.find('a.active').length > 0) {
+        $menu.attr('dropdown-active', 1);
+        $canvas.addClass('active')
+      }
+      else {
+        $menu.attr('dropdown-active', 0);
+        $canvas.removeClass('active')
+      }
+    }
+  }
+
 
 function round(number, precision) {
   var shift = function (number, precision) {
