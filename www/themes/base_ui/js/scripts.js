31f9bbc953992154ef382df977823cc6371d77ae
Isaac Bigsby Trogdon <mr@dyss.net>
2018-06-29 18:47:58 +0200

    Fixes for menu scroll issues.

--- www/themes/base_ui/js/scripts.js
+++ www/themes/base_ui/js/scripts.js
@@ -15,40 +15,26 @@
 
       function offCanvasMenuPosition() {
         // if (true) return;
-        $('[menu-mobile] > ul', context).once('offCanvasMenuPosition').each(function () {
+        $('[menu-mobile] > ul').each(function () {
           var $menu = $(this);
-          // console.log($menu);
-          // let status = $menu.attr('off-canvas-status');
-          let status = 0;
-          let offset = round($menu.outerWidth() * status, 0) + 1;
-          // Init position - translateX of menu-width.
-          // $menu.css({
-          //   // 'left': offset + 'px',
-          //   // 'transform': 'translateX(' + offset + 'px)',
-          //   'opacity': status,
-          // });
 
           function toggleMenu($menu, status = null) {
             if (status !== null) {
               status = 0;
-              console.log('override')
             }
             else {
               status = $menu.parent().attr('off-canvas-status');
               status = status == 0 ? 1 : 0;
             }
-            // invert status
-            let offset = round($menu.outerWidth() * status * -1, 0) + 1;
-            // $menu.velocity({
-            //   // 'translateX': (offset * -1) + 'px',
-            //   'opacity': status,
-            // }, {
-            //   duration: 50,
-            //   easing: 'easein',
-            // });
             $menu.parent().attr('off-canvas-status', status);
             $menu.parent().toggleClass('menu-closed');
-            $('body').toggleClass('off-canvas-menu-active');
+            if (status == 0) {
+              $('body').removeClass('off-canvas-menu-active');
+            }
+            else {
+              $('body').addClass('off-canvas-menu-active');
+              menuHeightNegotiator($menu, $)
+            }
             // let $logo = $('.site-branding');
             $canvas.toggleClass('active');
           }
@@ -61,34 +47,30 @@
           $canvas.click(function(e){
             toggleMenu($menu, 0);
           });
-
-
         });
       }
 
       offCanvasMenuPosition();
 
-      var waitForFinalEvent = (function() {
-        var timers = {};
-        return function(callback, ms, uniqueId) {
-          if (!uniqueId) {
-            uniqueId = "Don't call this twice without a uniqueId";
-          }
-          if (timers[uniqueId]) {
-            clearTimeout(timers[uniqueId]);
-          }
-          timers[uniqueId] = setTimeout(callback, ms);
-        };
-      })();
+      // var waitForFinalEvent = (function() {
+      //   var timers = {};
+      //   return function(callback, ms, uniqueId) {
+      //     if (!uniqueId) {
+      //       uniqueId = "Don't call this twice without a uniqueId";
+      //     }
+      //     if (timers[uniqueId]) {
+      //       clearTimeout(timers[uniqueId]);
+      //     }
+      //     timers[uniqueId] = setTimeout(callback, ms);
+      //   };
+      // })();
 
-      $(window).resize(function() {
-        // console.log('resizing');
-        waitForFinalEvent(function() {
-          offCanvasMenuPosition();
-          // $menu.attr('off-canvas-status', 0);
-        }, 500);
-        $('[menu-mobile]').addClass('menu-closed').attr('off-canvas-status', 0);
-      });
+      // $(window).resize(function() {
+      //   waitForFinalEvent(function() {
+      //     offCanvasMenuPosition();
+      //   }, 500);
+      //   $('[menu-mobile]').addClass('menu-closed').attr('off-canvas-status', 0);
+      // });
 
 
     }
@@ -228,21 +210,6 @@
     }
   }
 
-  // /**
-  //  * Custom UI for pageTabs menu behavior
-  //  */
-  // Drupal.behaviors.pageTabsUI = {
-  //   attach: function (context, settings) {
-  //     // $('[page-tabs-labels]', context).once('pageTabsUI').each(function () {
-  //     //   let $labels = $(this);
-  //     //   $labels.find('li').first().appendTo($(this)).addClass('hide-on-load');
-  //     //   $labels.find('a').click(function(){
-  //     //     $labels.find('li').removeClass('hide-on-load');
-  //     //   });
-  //     // });
-  //   }
-  // }
-
   /**
    * Init Masonry grids.
    */
@@ -273,13 +240,8 @@
 
         $menu.attr('dropdown-active', 0);
         $menu.find('li a').click(function(e){
-          // let $link = $(this);
           let $link = $(this).parent().children('a');
           let $submenu = $(this).parent().children('ul');
-          // console.log($link);
-          // console.log($submenu);
-          // If submenu exists under this link, toggle open.
-          // e.preventDefault();
           toggleDropdown($menu, $submenu, $link, e, null, $);
         });
 
@@ -300,46 +262,77 @@
         $(window).resize(function(e){
           toggleDropdown($('#main-navigation nav[menu-desktop]'), false, false, e, -1, $);
         });
-
-
       });
     }
   };
 
 })(jQuery);
 
+/**
+ *
+ */
 function toggleDropdown($menu, $submenu, $link, clickEvent, override = null, $) {
   var $canvas = $('#menu-canvas');
-  // var $canvas = document.getElementById('#menu-canvas');
+
+  // If override is set, close all dropdowns.
   if (override == -1) {
     $menu.find('a.active').removeClass('active');
     $menu.find('ul.submenu-open').removeClass('submenu-open');
     $menu.attr('dropdown-active', 0);
+    $('body').removeClass('dropdown-menu-active');
     $canvas.removeClass('active');
     return false;
   }
-  // console.log('submenu maybe')
-  // console.log($submenu);
-  if ($submenu.length != 0) {
+  else {
+    if ($submenu.length != 0) {
+      clickEvent.preventDefault();
 
-    clickEvent.preventDefault();
+      // Toggle out any currently active menus.
+      let $siblings = $link.parent().siblings('li');
+      $siblings.find('a.active').removeClass('active');
+      $siblings.find('ul.submenu-open').removeClass('submenu-open');
+      // Toggle active submenu.
+      $link.toggleClass('active');
+      $submenu.toggleClass('submenu-open');
+      // Global menu settings.
+      if($menu.find('a.active').length > 0) {
+        $menu.attr('dropdown-active', 1);
+        $canvas.addClass('active');
+        $('body').addClass('dropdown-menu-active');
+        menuHeightNegotiator($submenu, $);
+      }
+      else {
+        $menu.attr('dropdown-active', 0);
+        $canvas.removeClass('active');
+        $('body').removeClass('dropdown-menu-active');
+      }
+    }
+  }
 
-    // Toggle out any currently active menus.
-    let $siblings = $link.parent().siblings('li');
-    $siblings.find('a.active').removeClass('active');
-    $siblings.find('ul.submenu-open').removeClass('submenu-open');
-    // Toggle active submenu.
-    $link.toggleClass('active');
-    $submenu.toggleClass('submenu-open');
-    // Global menu settings.
-    if($menu.find('a.active').length > 0) {
-      $menu.attr('dropdown-active', 1);
-      $canvas.addClass('active')
-    }
-    else {
-      $menu.attr('dropdown-active', 0);
-      $canvas.removeClass('active')
-    }
+}
+
+/**
+ *
+ * @param {*} $submenu
+ * @param {*} $canvas
+ */
+function menuHeightNegotiator($submenu, $) {
+  // let y = $submenu.parents('nav').offset().top;
+  let $main = $('main');
+  let h = $submenu.outerHeight();
+  // Not sure why, but the mobile menu requires extra height to achive overflow scroll.
+  let o = $('window').outerWidth() <= 900 ? 130 : 0;
+  if ($main.outerHeight() >= $('window').outerHeight()) {
+    $main.css({
+      'height': h + o + 'px',
+    });
+  }
+  else {
+    let h = $(window).outerHeight();
+    // console.log('window height: ' + h);
+    $main.css({
+      'height': h - 100 + 'px',
+    });
   }
 }
 
